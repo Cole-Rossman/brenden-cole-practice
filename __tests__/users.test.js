1f77027ff7f4b00d449ef7b2be64fd629e706a3b
Cole Rossman <colerossman99@gmail.com>
2022-08-18 14:59:21 -0700

    returns an authenticated user test created, endpoint for /me created in controller, authenticate middleware created

--- __tests__/users.test.js
+++ __tests__/users.test.js
@@ -9,6 +9,17 @@ const mockUser = {
   password: 'testing321'
 };
 
+const registerAndLogin = async (userProps = {}) => {
+  const password = userProps.password ?? mockUser.password;
+
+  const agent = request.agent(app);
+
+  const user = await UserService.create({ ...mockUser, ...userProps });
+
+  const { email } = user;
+  await agent.post('/api/v1/users/sessions').send({ email, password });
+  return [agent, user];
+};
 
 
 describe('users routes testing', () => {
@@ -36,6 +47,18 @@ describe('users routes testing', () => {
     const res = await request(app).post('/api/v1/users/sessions').send({ email, password });
     expect(res.status).toBe(200);
     expect(res.body).toEqual({ message: 'Successfully signed in!' });
+  });
+
+  it('returns an authenticated user', async () => {
+    const [agent, user] = await registerAndLogin();
+
+    const mySession = await agent.get('/api/v1/users/me');
+
+    expect(mySession.body).toEqual({
+      ...user,
+      exp: expect.any(Number),
+      iat: expect.any(Number),
+    });
   });
   
 });
